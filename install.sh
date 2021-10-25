81bee06a48034b579ee746d44c343d2bfba7fe49
Eric Swanson <960576+ericrswanon@users.noreply.github.com>
2021-10-25 16:33:21 -0500

    Adding Folder if not existent

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo "Copying to ~/.local/share/xfce4/terminal/colorschemes"
+mkdir -p ~/.local/share/xfce4/terminal/colorschemes/
 cp zenburn.theme ~/.local/share/xfce4/terminal/colorschemes/
